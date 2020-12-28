2d2134ff96f46a7a7f90c2f86667472b5164abcb
parvada-maker <74596280+parvada-maker@users.noreply.github.com>
2020-12-28 12:35:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var ghost,ghostwalk;
+function preload() {
+  ghostwalk=loadAnimation("ghost1.png","ghost2.png","ghost3.png")
+
+}
+
+function setup(){
+createCanvas(400,400);
+ghost=createSprite(200,200,10,10);
+ ghost.addAnimation("scare",ghostwalk)
+}
+
+function draw(){
+  background('black')
+drawSprites();
+  
+}
